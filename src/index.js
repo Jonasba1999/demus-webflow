c4133f821213002922fe2256ead4ace96f2492c2
Jonas <jonasbal1999@gmail.com>
2024-07-03 17:52:17 +0300

    Smooth scroll fixed

--- src/index.js
+++ src/index.js
@@ -31,8 +31,6 @@ document.addEventListener("enable-scroll", enableScroll);
 // Lenis smooth scroll function
 let lenis;
 const smoothScroll = function () {
-	console.log("response from smoothscroll");
-	console.log(Webflow.env("editor"));
 	if (Webflow.env("editor") === undefined) {
 		lenis = new Lenis({
 			lerp: 0.1,
